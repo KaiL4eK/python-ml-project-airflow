0432a7f6d16a4e01d12effc420c44907b5a5141c
Aleksey Devyatkin <aleksey.devyatkin@waveaccess.global>
2025-01-21 18:46:41 +0300

    [main] Added sample dag with operator

--- dags/simple_dag_timer_op.py
+++ dags/simple_dag_timer_op.py
@@ -1,8 +1,9 @@
-"""Sample DAG."""
+"""Sample DAG with time operator."""
 
-from datetime import datetime
+from datetime import datetime, timedelta, timezone
 
 from airflow.decorators import dag
+from airflow.sensors.time_sensor import TimeSensorAsync
 
 from python_ml_project_airflow.airflow.tasks import (
     sample_sleep_task,
@@ -14,18 +15,26 @@ from python_ml_project_airflow.airflow.tasks import (
 
 
 @dag(
-    dag_id="simple-dag",
+    dag_id="simple-dag-timer-op",
     start_date=datetime(2025, 1, 1),  # noqa: DTZ001
     schedule_interval=None,
     catchup=False,
 )
 def simple_dag() -> None:
-    """Sample DAG."""
+    """Sample DAG with time operator."""
+    sample_async_timer_sensor = TimeSensorAsync(
+        task_id="timeout_after_second_date_in_the_future",
+        timeout=10,
+        soft_fail=True,
+        target_time=(datetime.now(tz=timezone.utc) + timedelta(minutes=1)).time(),
+    )
+
     (
         simple_task_taskflow()
         >> simple_task_operator
         >> sample_sleep_task(sleep_time=10)
         >> sample_sleep_task(sleep_time=4)
+        >> sample_async_timer_sensor
         >> show_logic_time_task()
         >> show_sample_variable()
     )
